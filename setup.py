0bd1e4227cf174e3badf0fb965503caa2732d64c
ahmed-tabib <ahmed.tabib0@gmail.com>
2024-06-10 15:15:47 +0100

    restructuring

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='vctk',
+    version='0.1.0',    
+    description='video creation toolkit',
+    url='https://github.com/ahmed-tabib/VideoCreationToolkit',
+    author='Ahmed Tabib',
+    packages=['vctk'],
+    install_requires=['ffmpeg-python'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
